8179fc08250aa099b00b2b46017fdf8f224017f5
sugarman1983 <jsato.jtoz@gmail.com>
2023-02-21 03:40:35 +0900

    マイグレーションファイルを実行

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id()->index();
+            $table->string('name', 100)->index();
+            $table->string('phone', 11);
+            $table->string('postcode', 7);
+            $table->string('address', 255);
+            $table->string('email', 255)->unique()->index();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('password', 255);
+            $table->rememberToken('remember_token', 100)->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
