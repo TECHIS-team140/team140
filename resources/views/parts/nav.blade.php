b35df85d4a49a5a15f5efa2d6ef1d053a650a81d
im-09mi <116474060+im-09mi@users.noreply.github.com>
2023-03-06 22:22:18 +0900

    20230306-matukuma

--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -1,10 +1,20 @@
+<style>
+.nav-link{
+  text-decoration: none;
+    transition: text-shadow .5s;
+}
+
+.nav-link:hover{
+  text-shadow: 0 4px 4px;
+}
+</style>
 <nav class="navbar navbar-expand-sm navbar-dark bg-secondary p-1">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="">商品管理システム</a>
         <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav me-auto">
             @auth
 @if (auth()->user()->role === 1)
                 <li class="nav-item active">
@@ -25,6 +35,6 @@
                     <a class="nav-link" href="/">ログアウト</a>
                 </li>
             </ul>
-                    <p class = "usertext">[ようこそ、{{Auth::user()->name }}さん]</p>
+                    <p style = "color:azure; margin-bottom: 2px;">[ようこそ、{{Auth::user()->name }}さん]</p>
         </div>
     </nav> 
